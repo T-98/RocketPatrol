9ed4797e1f50163d499bf835c8e87c53b4d88e9a
T-98 <dkhare@ucsc.edu>
2021-05-20 02:08:39 -0700

    fixed restart bug

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -77,6 +77,9 @@ class Play extends Phaser.Scene {
     update() {
         // check key input for restart
         if (this.gameOver && Phaser.Input.Keyboard.JustDown(keyR)) {
+            this.scene.restart();
+        }
+        if (this.gameOver && Phaser.Input.Keyboard.JustDown(keyLEFT)) {
             this.scene.start("menuScene");
         }
         this.starfield.tilePositionX -= 4;
